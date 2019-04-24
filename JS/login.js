8b5eef9a15fd925dc0fb0349f9264f8f1789f8d2
Zoran Tomic <44097256+zoran978@users.noreply.github.com>
2019-04-24 02:29:21 +0200

    Add files via upload

---
+++ JS/login.js
@@ -0,0 +1,18 @@
+// let usernamex = 'toto';
+// let passx = 1234
+
+// let userInpName = $('#usernamett').val()
+// let userInpPass = $('#passww').val()
+
+// $(document).on('click', "#buttn", function () {
+//     if ( userInpPass == passx && userInpName == usernamex) {
+//         $('#myNav').css('width', '0%')
+//         $('.afterlog').css('display', 'block')
+//         $('#logbt').css('display', 'none')
+//         $('.overlay').append('<button class="btn btn-outline-primary" id="logoutbtn">Log Out</button>')
+//         $('#myNav').css('width', '100%')
+//     }
+//     else {
+//         alert('Invalid username or password')
+//     }
+// })
